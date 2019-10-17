61eeadd378999a316acf635a43ff7cc38a1b299e
Roger <20207593@qq.com>
2019-10-17 08:13:02 +0800

    plus-one.py

--- Week 01/id_309/plus-one.py
+++ Week 01/id_309/plus-one.py
@@ -42,7 +42,7 @@ class Solution4:
             result.insert(0, 1)
             return result
 
-class Solution:
+class Solution5:
     def plusOne(self, digits: List[int]) -> List[int]:
         if len(digits) == 1 and digits[0] == 9:
             return [1, 0]
@@ -53,3 +53,13 @@ class Solution:
             digits[-1] = 0
             digits[:-1] = self.plusOne(digits[:-1])
             return digits
+
+class Solution:
+    def plusOne(self, digits):
+        for i in range(len(digits)):
+            if digits[~i] < 9:
+                digits[~i] += 1
+                return digits
+            elif digits[~i] == 9:
+                digits[~i] = 0
+        return [1] + [0] * len(digits)
