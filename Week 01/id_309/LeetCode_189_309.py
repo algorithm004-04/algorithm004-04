553d7e9151262f468d8b3f743cbb722532a696d6
Roger <20207593@qq.com>
2019-10-19 21:18:17 +0800

    LeetCode_189_309.py

--- Week 01/id_309/LeetCode_189_309.py
+++ Week 01/id_309/LeetCode_189_309.py
@@ -36,10 +36,8 @@ class Solution3:
         nums[:] = nums[n-k:] + nums[:n-k]
 
 class Solution:
+    @pysnooper.snoop()
     def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
         lenth = len(nums)
         k = k % lenth
         if lenth <= 1 or k <= 0:
@@ -52,8 +50,15 @@ class Solution:
             nums[first],nums[next] = nums[next],nums[first]
             first += 1
             next += 1
+            # 二号指针走k步，就碰到最后，就回到分界线，因为分界线分割了数组
+            # 分界线右侧是一个循环替换。
             if next == last:
                 next = middle
+            # 如果一号指针碰到分界线，就把分界线改成二号指针的位置
+            # 这个if-else就是把数组切分了。以13右移5举例：
+            # 先排好了5+3=8，则分界线变动。数组切出来一个5右移2
+            # 排好5右移2，还剩13-8-2=3
+            # 继续排3右移2，排到最后，二号指针和一号指针相遇。
             elif first == middle:
                 middle = next
 
