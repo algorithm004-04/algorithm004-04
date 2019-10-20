34cebe6980781e16cb6095d350dd928cb8ee97fd
Roger <20207593@qq.com>
2019-10-20 10:58:23 +0800

    LeetCode_189_309.py

--- Week 01/id_309/LeetCode_88_309.py
+++ Week 01/id_309/LeetCode_88_309.py
@@ -18,7 +18,20 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        
+        if n == 0:
+            return None
+        pz = len(nums1) - 1
+        p1 = m - 1
+        p2 = n - 1
+        while pz >=0 and p2 >= 0:
+            if p1 < 0 or nums2[p2] >= nums1[p1]:
+                nums1[pz] =  nums2[p2]
+                pz -= 1
+                p2 -= 1
+            elif nums1[p1] > nums2[p2]:
+                nums1[pz] = nums1[p1]
+                pz -= 1
+                p1 -= 1
 
 if __name__ == "__main__":
     nums1 = [7,2,3,0,0,0]
