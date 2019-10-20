730e5930b466aab189ca8049300dd0434228ddaf
wujunze <itwujunze@163.com>
2019-10-21 00:05:30 +0800

    optimize 644 code

--- Week 01/id_199/LeetCode_641_199.java
+++ Week 01/id_199/LeetCode_641_199.java
@@ -1,109 +1,97 @@
 package com.wujunze.Design_Circular_Dequeue;
 
 class MyCircularDeque {
-    int[] circular;
-    int size;
-    int head;
-    int rear;
+    int[] myqueue ;
+    int front;//队头指针
+    int rear;//队尾指针
+    int size;//队列当前的大小
+    int capacity;//队列的容量
 
-    /**
-     * Initialize your data structure here. Set the size of the deque to be k.
-     */
+    /** Initialize your data structure here. Set the size of the deque to be k. */
     public MyCircularDeque(int k) {
-        circular = new int[k + 1];
-        size = k + 1;
-        head = rear = 0;
-        for (int i = 0; i < size; i++) {
-            circular[i] = -1;
+        this.myqueue = new int[k];
+        this.front = 0;
+        this.rear = 0;
+        this.size = 0;
+        this.capacity = k;
+    }
+
+    /** Adds an item at the front of Deque. Return true if the operation is successful. */
+    public boolean insertFront(int value) {
+        if(rear==front && size == capacity) return  false;//如果队列满，插入失败
+        else {
+            front = (front + capacity -1)% capacity;
+            myqueue[front] = value;
+            size++;
+            return true;
         }
     }
 
-    /**
-     * Adds an item at the front of Deque. Return true if the operation is successful.
-     */
-    public boolean insertFront(int value) {
-        if (isFull())
-            return false;
-        head = Math.floorMod(head - 1, size);
-        circular[head] = value;
-        return true;
-    }
-
-    /**
-     * Adds an item at the rear of Deque. Return true if the operation is successful.
-     */
+    /** Adds an item at the rear of Deque. Return true if the operation is successful. */
     public boolean insertLast(int value) {
-        if (isFull())
-            return false;
-        circular[rear] = value;
-        rear = Math.floorMod(rear + 1, size);
-        return true;
+        if(rear==front && size == capacity) return  false;//如果队列满，插入失败
+        else {
+            myqueue[rear] = value;
+            rear = (rear+1+capacity)%capacity;
+            size++;
+            return true;
+        }
+
     }
 
-    /**
-     * Deletes an item from the front of Deque. Return true if the operation is successful.
-     */
+    /** Deletes an item from the front of Deque. Return true if the operation is successful. */
     public boolean deleteFront() {
-        if (isEmpty())
-            return false;
-        circular[head] = -1;
-        head = Math.floorMod(head + 1, size);
-        return true;
+        if( rear == front && size == 0) return false;
+        else {
+            front = (front+1) % capacity;
+            size--;
+            return true;
+        }
+
     }
 
-    /**
-     * Deletes an item from the rear of Deque. Return true if the operation is successful.
-     */
+    /** Deletes an item from the rear of Deque. Return true if the operation is successful. */
     public boolean deleteLast() {
-        if (isEmpty())
-            return false;
-        rear = Math.floorMod(rear - 1, size);
-        circular[rear] = -1;
-        return true;
+        if( rear == front && size == 0) return false;
+        else {
+            rear = (rear - 1 + capacity) % capacity;
+            size--;
+            return true;
+        }
+
     }
 
-    /**
-     * Get the front item from the deque.
-     */
+    /** Get the front item from the deque. */
     public int getFront() {
-        if (isEmpty())
-            return -1;
-        return circular[head];
+        if((rear == front) && size==0) return -1;
+        else {
+            int frontE = myqueue[front];
+            //front = (front + 1) % capacity;
+            //size--;
+            return frontE;
+        }
     }
 
-    /**
-     * Get the last item from the deque.
-     */
+    /** Get the last item from the deque. */
     public int getRear() {
-        if (isEmpty())
-            return -1;
-        return circular[Math.floorMod(rear - 1, size)];
+        if((rear == front) && size==0) return -1;
+        else {
+            int rearE = myqueue[(rear-1+capacity)%capacity];
+            // rear = (rear - 1 +capacity)%capacity;
+            //size--;
+            return rearE;
+        }
+
     }
 
-    /**
-     * Checks whether the circular deque is empty or not.
-     */
+    /** Checks whether the circular deque is empty or not. */
     public boolean isEmpty() {
-        return head == rear;
+        return (rear == front) && size==0;
     }
 
-    /**
-     * Checks whether the circular deque is full or not.
-     */
+    /** Checks whether the circular deque is full or not. */
     public boolean isFull() {
-        return Math.floorMod(rear + 1, size) == head;
+        return rear==front && size == capacity;
+
     }
 }
-
-/**
- * Your MyCircularDeque object will be instantiated and called as such:
- * MyCircularDeque obj = new MyCircularDeque(k);
- * boolean param_1 = obj.insertFront(value);
- * boolean param_2 = obj.insertLast(value);
- * boolean param_3 = obj.deleteFront();
- * boolean param_4 = obj.deleteLast();
- * int param_5 = obj.getFront();
- * int param_6 = obj.getRear();
- * boolean param_7 = obj.isEmpty();
- * boolean param_8 = obj.isFull();
- */
