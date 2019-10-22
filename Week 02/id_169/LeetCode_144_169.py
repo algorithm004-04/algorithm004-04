3a499c62a5a0a15936901288f18fd68f20bca318
huang-yi <coodeer@163.com>
2019-10-22 14:25:00 +0800

    LeetCode#144

---
+++ Week 02/id_169/LeetCode_144_169.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root: return []
+        return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+
+
+root = TreeNode(1)
+root.right = TreeNode(2)
+root.right.left = TreeNode(3)
+
+print(Solution().preorderTraversal(root))
