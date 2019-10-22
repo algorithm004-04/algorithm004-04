927ce2e3ee98c56f1dcffb37f2633fb2d63d5ccb
Echo <327985860@qq.com>
2019-10-22 10:50:26 +0800

    commit 564-Week 01 homework

---
+++ Week 01/id_564/LeetCode_88_564.php
@@ -0,0 +1,38 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums1
+     * @param Integer $m
+     * @param Integer[] $nums2
+     * @param Integer $n
+     * @return NULL
+     */
+    function merge(&$nums1, $m, $nums2, $n) {
+        $i = $m - 1;
+        $j = $n - 1; 
+        $k = $m + $n - 1;
+        while ($i >= 0 && $j >= 0) {
+            if ($nums2[$j] > $nums1[$i]) {
+                $nums1[$k--] = $nums2[$j--];
+            } else {
+                $nums1[$k--] = $nums1[$i--];
+            }
+        }
+        while ($j >= 0) {
+            $nums1[$k--] = $nums2[$j--];
+        }
+    }
+}
+
+$nums = [-1, -100, 3, 99];
+$k = 2;
+
+$s = new Solution();
+$s->mergeTwoLists($nums, $k);
+print_r($nums);
+
+?>
+
+
