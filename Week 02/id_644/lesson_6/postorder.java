b3307c0b262d91423e91948747c568edcb459583
Bradsunbin <brad_sun@126.com>
2019-10-23 17:24:13 +0800

    1022

---
+++ Week 02/id_644/lesson_6/postorder.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class postorder {
+
+    public static void main(String[] args) {
+
+    }
+    List<Integer> res = new ArrayList<Integer>();
+    public List<Integer> postorder(Node root) {
+        helper(root);
+        return res;
+    }
+    //递归
+    public void helper(Node root) {
+        if(root == null) {
+            return;
+        }
+        /*for(Node child: root.children) {
+            helper(child);  两层方法调用 iterater 和 递归结合效率相对低
+        }\
+        */
+        int s = root.children.size();
+        for(int i = 0; i < s; i++) {
+            helper(root.children.get(i));
+        }
+        res.add(root.val);
+    }
+    //模拟
+
+
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {}
+
+        public Node(int _val,List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+}
