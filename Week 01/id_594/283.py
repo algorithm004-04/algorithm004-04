887440212e01f4f78cbd4f008629452d583a8dda
Water <water@WaterdeiMac.local>
2019-10-27 15:22:05 +0800

    欠042

--- Week 01/id_594/283.py
+++ Week 01/id_594/283.py
@@ -13,7 +13,10 @@ def moveZeroes2(nums):
     zero = 0  # records the position of "0"
     for i in range(len(nums)):
         if nums[i] != 0:
-            nums[i], nums[zero] = nums[zero], nums[i]
+            nums[i], nums[zero] = nums[zero], nums[i]     # 这个地方明显有毒，和下面的方式是不对等的
+            # nums[i] = nums[zero]
+            # nums[zero] = nums[i]
+            print(" nums[i]:", nums[i], '\n', "nums[zero]:", nums[zero])
             zero += 1
     return nums
 
@@ -30,6 +33,5 @@ def moveZeroes3(nums):
     return nums
 
 
-test_arrry = [1, 0, 3, 5]
-
-print(moveZeroes3(test_arrry))
+test_arrry = [0, 1, 0, 3, 12]
+print(moveZeroes2(test_arrry))
