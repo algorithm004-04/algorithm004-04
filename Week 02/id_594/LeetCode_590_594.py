dae7ccff45331d4f7e6c9d7f44c475cd6cba234e
Water <water@WaterdeiMac.local>
2019-10-30 19:38:00 +0800

    tree

--- Week 02/id_594/LeetCode_590_594.py
+++ Week 02/id_594/LeetCode_590_594.py
@@ -12,7 +12,7 @@ def postorder(root: 'Node'):        # 这个解题方式，还不理解是什么
 
     stack = [root]
     while stack:
-        temp = stack.pop()
-        res.append(temp.val)
-        stack.extend(temp.chlidren)
+        root = stack.pop()
+        if not root:
+            
     return res[:: -1]
