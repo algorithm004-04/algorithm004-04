bfe68af57e069770dce8958d129ec1b29434b456
wxy <597123718@qq.com>
2019-11-03 23:52:54 +0800

    134-week03

---
+++ Week 03/id_134/leetcode_200_134.java
@@ -0,0 +1,111 @@
+//给定一个由 '1'（陆地）和 '0'（水）组成的的二维网格，计算岛屿的数量。一个岛被水包围，并且它是通过水平方向或垂直方向上相邻的陆地连接而成的。你可以假设网格的四个边均被水包围。
+//
+// 示例 1:
+//
+// 输入:
+//11110
+//11010
+//11000
+//00000
+//
+//输出: 1
+//
+//
+// 示例 2:
+//
+// 输入:
+//11000
+//11000
+//00100
+//00011
+//
+//输出: 3
+//
+// Related Topics 深度优先搜索 广度优先搜索 并查集
+
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    public void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+            return;
+        }
+
+        grid[r][c] = '0';
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
+    //广度优先
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    grid[r][c] = '0'; // mark as visited
+                    Queue<Integer> nei***ors = new LinkedList<>();
+                    nei***ors.add(r * nc + c);
+                    while (!nei***ors.isEmpty()) {
+                        int id = nei***ors.remove();
+                        int row = id / nc;
+                        int col = id % nc;
+                        if (row - 1 >= 0 && grid[row-1][col] == '1') {
+                            nei***ors.add((row-1) * nc + col);
+                            grid[row-1][col] = '0';
+                        }
+                        if (row + 1 < nr && grid[row+1][col] == '1') {
+                            nei***ors.add((row+1) * nc + col);
+                            grid[row+1][col] = '0';
+                        }
+                        if (col - 1 >= 0 && grid[row][col-1] == '1') {
+                            nei***ors.add(row * nc + col-1);
+                            grid[row][col-1] = '0';
+                        }
+                        if (col + 1 < nc && grid[row][col+1] == '1') {
+                            nei***ors.add(row * nc + col+1);
+                            grid[row][col+1] = '0';
+                        }
+                    }
+                }
+            }
+        }
+
+        return num_islands;
+    }
+
+    //深度优先
+//    public int numIslands(char[][] grid) {
+//        if (grid == null || grid.length == 0) {
+//            return 0;
+//        }
+//
+//        int nr = grid.length;
+//        int nc = grid[0].length;
+//        int num_islands = 0;
+//        for (int r = 0; r < nr; ++r) {
+//            for (int c = 0; c < nc; ++c) {
+//                if (grid[r][c] == '1') {
+//                    ++num_islands;
+//                    dfs(grid, r, c);
+//                }
+//            }
+//        }
+//
+//        return num_islands;
+//    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
