d950df7eef077e0a7f1cb1585c55c6317eb4f637
Arvin Shaffer <icuipi919@gmail.com>
2019-11-10 15:45:45 +0800

    039-Week 03

---
+++ Week 03/id_039/LeetCode_127_039.py
@@ -0,0 +1,15 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        wordList = set(wordList)
+        queue = collections.deque([[beginWord, 1]])
+        while queue:
+            word, length = queue.popleft()
+            if word == endWord:
+                return length
+            for i in range(len(word)):
+                for c in 'abcdefghijklmnopqrstuvwxyz':
+                    next_word = word[:i] + c + word[i+1:]
+                    if next_word in wordList:
+                        wordList.remove(next_word)
+                        queue.append([next_word, length + 1])
+        return 0
