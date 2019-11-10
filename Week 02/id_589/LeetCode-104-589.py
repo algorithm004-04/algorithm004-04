1cfdfeb5a036a03cc32e21518bc8fa861a78b457
seamoontime <hy19900815@126.com>
2019-11-10 10:38:00 +0800

    add leetcode 104

---
+++ Week 02/id_589/LeetCode-104-589.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode.cn id=104 lang=python3
+#
+# [104] 二叉树的最大深度
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        else:
+            left_height = self.maxDepth(root.left)
+            right_height = self.maxDepth(root.right)
+            return max(left_height, right_height) + 1
+        
+# @lc code=end
+
