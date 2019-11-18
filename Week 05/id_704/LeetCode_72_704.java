191639d08cdc93c88bcf3e761e29655cf4266390
yuezhiping <yuezhiping@bonc.com.cn>
2019-11-18 18:19:33 +0800

    704-Week 05

--- Week 05/id_704/LeetCode_72_704.java
+++ Week 05/id_704/LeetCode_72_704.java
@@ -11,6 +11,7 @@ class Solution {
                         1 + Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1])) : dp[i-1][j-1];
             }
         }
+
         return dp[word1.length()][word2.length()];
     }
 }
