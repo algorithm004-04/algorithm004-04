30dc9bb66d879dfb5ccba431dc8e3aa660086722
mengdebiao <1006760526@qq.com>
2019-11-24 20:45:13 +0800

    Week 06

---
+++ Week 06/id_034/LeetCode_208_034.java
@@ -0,0 +1,81 @@
+/**
+ * LeetCode_208_034
+ *
+ * @Author blackberry
+ * @Date 2019/11/24 6:25 PM
+ **/
+class Trie {
+
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        char[]  chs = word.toCharArray();
+        for (char c : chs) {
+            if (!node.containsKey(c)) {
+                node.put(c, new TrieNode());
+            }
+            node = node.get(c);
+        }
+        node.setEnd();
+    }
+
+    public boolean search(String word) {
+        TrieNode node = searchPrefix(word);
+        return node != null && node.isEnd();
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = searchPrefix(prefix);
+        return node != null;
+    }
+
+    private TrieNode searchPrefix(String prefix) {
+        TrieNode node = root;
+        char[]  chs = prefix.toCharArray();
+        for (char c : chs) {
+            if (node.containsKey(c)) {
+                node = node.get(c);
+            } else {
+                return null;
+            }
+        }
+        return node;
+    }
+
+
+    class TrieNode {
+        private TrieNode[] links;
+        private final int R = 26;
+
+        private boolean isEnd;
+
+        public TrieNode() {
+            links = new TrieNode[R];
+        }
+
+        public boolean containsKey(char ch) {
+            return links[ch - 'a'] != null;
+        }
+
+        public TrieNode get(char ch) {
+            return links[ch - 'a'];
+        }
+
+        public void put(char ch, TrieNode node) {
+            links[ch - 'a'] = node;
+        }
+
+        public void setEnd() {
+            isEnd = true;
+        }
+
+        public boolean isEnd() {
+            return isEnd;
+        }
+    }
+}
---
+++ Week 06/id_034/LeetCode_547_034.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2019 Baidu, Inc. All Rights Reserved.
+ */
+
+/**
+ * LeetCode_547_034
+ *
+ * @Author mengdebiao@duxiaoman.com
+ * @Date 2019/11/24 7:22 PM
+ **/
+public class LeetCode_547_034 {
+
+    // 并查集
+    public int findCircleNum(int[][] M) {
+
+        int n = M.length;
+        UnionFind unionFind = new UnionFind(n);
+
+        for (int i = 0; i < n - 1; i ++) {
+            for (int j = i + 1; j < n; j ++) {
+                if (M[i][j] == 1) {
+                    unionFind.union(i, j);
+                }
+            }
+        }
+        return unionFind.count;
+    }
+
+    class UnionFind {
+        private int[] parent;
+        private int count = 0;
+
+        public UnionFind(int n) {
+            parent = new int[n];
+            for (int i = 0; i < n; i ++) {
+                parent[i] = i;
+            }
+            count = n;
+        }
+
+        public int find(int p) {
+            while (p != parent[p]) {
+                parent[p] = parent[parent[p]];
+                p = parent[p];
+            }
+            return p;
+        }
+
+        public void union(int p, int q) {
+            int rootP = find(p);
+            int rootQ = find(q);
+
+            if (rootP == rootQ) {
+                return;
+            }
+            parent[rootP] = rootQ;
+            count --;
+        }
+    }
+
+}
