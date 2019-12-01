34c3c50ac9355b2fb6744b2630c65612f6c38495
pickmonster <pickmonster@foxmail.com>
2019-12-01 23:04:50 +0800

    fix

---
+++ Week 07/id_339/Leecode_493_339.java
@@ -0,0 +1,45 @@
+package com.codertoy.week07;
+
+public class Leecode_493_339 {
+    public int ret;
+
+    public int reversePairs(int[] nums) {
+        ret = 0;
+        mergeSort(nums, 0, nums.length - 1);
+        return ret;
+    }
+
+    public void mergeSort(int[] nums, int left, int right) {
+        if (right <= left) {
+            return;
+        }
+        int middle = left + (right - left) / 2;
+        mergeSort(nums, left, middle);
+        mergeSort(nums, middle + 1, right);
+
+        //count elements
+        int count = 0;
+        for (int l = left, r = middle + 1; l <= middle; ) {
+            if (r > right || (long) nums[l] <= 2 * (long) nums[r]) {
+                l++;
+                ret += count;
+            } else {
+                r++;
+                count++;
+            }
+        }
+
+        //merge sort
+        int[] temp = new int[right - left + 1];
+        for (int l = left, r = middle + 1, k = 0; l <= middle || r <= right; ) {
+            if (l <= middle && ((r > right) || nums[l] < nums[r])) {
+                temp[k++] = nums[l++];
+            } else {
+                temp[k++] = nums[r++];
+            }
+        }
+        for (int i = 0; i < temp.length; i++) {
+            nums[left + i] = temp[i];
+        }
+    }
+}
