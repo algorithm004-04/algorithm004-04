fcb2d4423a0767e8f0f2886ef07ca17b6d1ce671
李琦 <753973775@qq.com>
2019-12-09 12:11:40 +0800

    add week08 homework

---
+++ Week 08/id_749/LeetCode_5_749.java
@@ -0,0 +1,27 @@
+class Sulation {
+	public boolean isPalindromic(String s) {
+		int len = s.length();
+		for (int i = 0; i < len / 2; i++) {
+			if (s.charAt(i) != s.charAt(len - i - 1)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+// 暴力解法
+public String longestPalindrome(String s) {
+    String ans = "";
+    int max = 0;
+    int len = s.length();
+    for (int i = 0; i < len; i++)
+        for (int j = i + 1; j <= len; j++) {
+            String test = s.substring(i, j);
+            if (isPalindromic(test) && test.length() > max) {
+                ans = s.substring(i, j);
+                max = Math.max(max, ans.length());
+            }
+        }
+    return ans;
+}
+}
