db5558b45418d12adc077f887f7bc3c9540ca1d7
TzuchiLin <kaylin7938@gmail.com>
2019-12-21 18:07:10 +0800

    Create LeetCode_208_219.java

---
+++ Week 06/id_219/LeetCode_208_219.java
@@ -0,0 +1,20 @@
+class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    // Inserts a word into the trie.
+    public void insert(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char currentChar = word.charAt(i);
+            if (!node.containsKey(currentChar)) {
+                node.put(currentChar, new TrieNode());
+            }
+            node = node.get(currentChar);
+        }
+        node.setEnd();
+    }
+}
