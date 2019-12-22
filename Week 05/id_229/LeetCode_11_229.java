1a0503b530dc220ac18fc608128839439c55e1dd
lypop <lining18330115795@gmail.com>
2019-12-22 08:26:50 +0800

    229-Week 05

---
+++ Week 05/id_229/LeetCode_11_229.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Administrator on 2019/12/15 0015.
+ */
+public class LeetCode_11_229_盛最多水的容器 {
+
+
+    public int maxArea(int[] height) {
+        int maxarea = 0, l = 0, r = height.length - 1;
+        while (l < r) {
+            maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return maxarea;
+    }
+
+}
